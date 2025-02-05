346b5d388f8d32ea3f91792851e0a7d60cb0cce5
Jonathan Eleiott <jonathan.eleiott@fullstackacademy.com>
2025-02-04 23:26:14 -0500

    update PORT

--- server.cjs
+++ server.cjs
@@ -50,6 +50,8 @@ app.post('/api/v1/products', (req, res, next) => {
   res.send(products);
 });
 
-app.listen(3000, () => {
-  console.log(`Listening on PORT 3000`)
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on PORT ${PORT}`);
 });
